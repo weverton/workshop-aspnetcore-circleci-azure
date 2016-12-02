a83b5acdeba4a276f065fd0b665f8cadac30409e
Weverton Gomes <weverton@gmail.com>
2016-12-01 23:11:46 -0200

    Criado novo teste

---
+++ test/UnitTest/Controllers/ValuesControllerIntegrationTest.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace UnitTest
+{
+    public class ValuesControllerIntegrationTest : BaseIntegrationTest
+    {
+        private const string BaseUrl = "/api/values";
+        public ValuesControllerIntegrationTest(BaseTestFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public async Task GetShouldReturnValues()
+        {
+            var response = await Client.GetAsync(BaseUrl);
+            response.EnsureSuccessStatusCode();
+
+            var datastring = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<List<string>>(datastring);
+
+            Assert.Equal(data.Count, 3);
+            Assert.Contains(data, x => x == "@wevertongomes");
+        }
+    }
+}
